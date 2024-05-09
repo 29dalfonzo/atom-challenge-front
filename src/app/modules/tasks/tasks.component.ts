e1c72b316f257043732105dc93d487c2bfc178fe
Dalfonzo <29dalfonzo@gmail.com>
2024-05-09 15:43:06 -0400

    Add edit and delete buttons to task options

--- src/app/modules/tasks/tasks.component.ts
+++ src/app/modules/tasks/tasks.component.ts
@@ -9,6 +9,7 @@ import { MatCardModule } from "@angular/material/card";
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from '@angular/material/list';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -20,11 +21,13 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   styleUrls: ['./tasks.component.scss'],
   imports: [CommonModule, MatButtonModule, MatCardModule,
     MatFormFieldModule, MatInputModule,
-    MatProgressBarModule, MatDividerModule, MatListModule, ReactiveFormsModule, MatCheckboxModule, MatTooltipModule],
+    MatProgressBarModule, MatDividerModule, MatListModule, ReactiveFormsModule, MatCheckboxModule,
+    MatTooltipModule, MatIconModule],
   standalone: true,
 })
 export class TasksComponent {
   form: FormGroup = new FormGroup({
+    id: new FormControl(''),
     title: new FormControl('', [Validators.required]),
     description: new FormControl('', [Validators.required]),
     done: new FormControl(false),
@@ -57,8 +60,20 @@ export class TasksComponent {
   submit() {
     console.log(this.form.value);
     this.loading = true;
-    setTimeout(() => {
-      console.log('Task created successfully');
+    // valida si el id existe en el arreglo
+    // si existe actualiza el objeto
+    // si no existe lo agrega
+    const findTask = this.tasks.find((t) => t.id === this.form.value.id);
+    if (findTask) {
+      this.tasks = this.tasks.map((t) => {
+        if (t.id === this.form.value.id) {
+          t.title = this.form.value.title;
+          t.description = this.form.value.description;
+          t.done = this.form.value.done;
+        }
+        return t;
+      });
+    } else {
       this.tasks.push({
         id: this.tasks.length + 1,
         title: this.form.value.title,
@@ -66,13 +81,28 @@ export class TasksComponent {
         date: new Date(),
         done: this.form.value.done
       });
-      this.loading = false;
-      this.resetFormt();
-    }, 2000);
+    }
+    this.loading = false;
+    this.resetFormt();
   }
 
   changeTask(task: Task):void {
     task.done = !task.done;
+  }
+
+  editTask(task: Task): void {
+    console.log('Edit task', task);
+    this.form.setValue({
+      title: task.title,
+      description: task.description,
+      done: task.done,
+      id: task.id
+    });
+  }
+
+  deleteTask(task: Task): void {
+    console.log('Delete task', task);
+    this.tasks = this.tasks.filter((t) => t.id !== task.id);
   }
 
   resetFormt() {
