b6d23baf9888bccafc7c0af82b1c1616a68fa04c
Dalfonzo <29dalfonzo@gmail.com>
2024-05-08 21:01:41 -0400

    setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { LoginComponent } from './modules/login/login.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
